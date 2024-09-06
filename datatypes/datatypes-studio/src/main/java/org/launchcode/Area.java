6c86e009dc126c5d06becbe04ecb66cf21d78cdb
craigfili <craig.filipowicz@gmail.com>
2024-09-05 19:46:46 -0500

    Studio in progress

---
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -0,0 +1,20 @@
+package org.launchcode;
+
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+//        final double PI = 3.14159;
+        double radius;
+        double area;
+
+        System.out.println("Enter a number for the radius: ");
+        radius = input.nextDouble();
+//        area = PI * radius * radius;
+        area = Circle.getArea(radius);
+
+        System.out.println("The area of a circle of radius " + radius + " is: " + area);
+
+    }
+}
